9a2de6841b4816f55f9e73d61192058510055434
Tom Victor <vjtomvictor@icloud.com>
2021-05-08 17:36:31 +0530

    Queue Based Threading (Recommended pattern)

--- thumbnail_maker.py
+++ thumbnail_maker.py
@@ -4,6 +4,8 @@ import os
 import logging
 from urllib.parse import urlparse
 from urllib.request import urlretrieve
+from threading import Thread
+from queue import Queue
 
 import PIL
 from PIL import Image
@@ -11,9 +13,11 @@ from PIL import Image
 from source import IMG_URLS
 
 
-import threading
-
 logging.basicConfig(filename='logfile.log',level=logging.DEBUG)
+
+
+
+# Consumer/Producer pattern
 
 
 class ThumbnailMakerService(object):
@@ -21,62 +25,31 @@ class ThumbnailMakerService(object):
         self.home_dir = home_dir
         self.input_dir = self.home_dir + os.path.sep + 'incoming'
         self.output_dir = self.home_dir + os.path.sep + 'outgoing'
-
-        self.download_bytes = 0
-        self.dl_loack = threading.Lock()
-        max_concurrebt_dl = 4
-        self.dl_sem = threading.Semaphore(max_concurrebt_dl)
-
-    def download_image(self,url):
-        # download he image and store it into local folder
-        self.dl_sem.acquire()
-        try:
-            logging.info("Downloading image " + url)
-            img_filename = urlparse(url).path.split('/')[-1]
-
-            dest_path = self.input_dir + os.path.sep + img_filename
-            urlretrieve(url, dest_path)
-            image_size = os.path.getsize(dest_path)
-            with self.dl_loack:
-                self.download_bytes += image_size
-            logging.info(f"image size : {image_size}")
-            logging.info(f"total size : {self.download_bytes}")
-            logging.info("Downloaded " + url)
-        finally:
-            self.dl_sem.release()
-        
+        self.img_queue = Queue()
+        self.dl_queue = Queue()
 
 
-    def download_images(self, img_url_list):
-        # validate inputs
-        if not img_url_list:
-            return
-        os.makedirs(self.input_dir, exist_ok=True)
-        
-        logging.info("beginning image downloads")
-
-        start = time.perf_counter()
-        threads = []
-        for url in img_url_list:
-            # download each image and save to the input dir 
-            img_filename = urlparse(url).path.split('/')[-1]
-            thread = threading.Thread(target=self.download_image,args=(url,))
-            thread.start()
-            threads.append(thread)
-        
-        logging.info(f"total threads : {len(threads)}")
-
-        for thread in threads:
-            thread.join()
-        
-        end = time.perf_counter()
-
-        logging.info("downloaded {} images in {} seconds".format(len(img_url_list), end - start))
+    def download_image(self):
+        while not self.dl_queue.empty():
+            try:
+                url = self.dl_queue.get(block=False) #block=False, will raise exception if Q happented to be empty (Somehow).
+                # download each image and save to the input dir 
+                img_filename = urlparse(url).path.split('/')[-1]
+                urlretrieve(url, self.input_dir + os.path.sep + img_filename)
+                self.img_queue.put(img_filename)
+                self.dl_queue.task_done() #mark Q as done for every url, when completed
+            # Thread Gurding
+            except Queue.Empty:
+                """This is a safety mechanisam.
+                
+                There is a possibility of while get true and queue
+                get empty since we have multiple threads running at the same time.
+                """
+                print("Queue is empty")
+                logging.info("Queue is empty")
 
     def perform_resizing(self):
-        # validate inputs
-        if not os.listdir(self.input_dir):
-            return
+
         os.makedirs(self.output_dir, exist_ok=True)
 
         logging.info("beginning image resizing")
@@ -84,22 +57,28 @@ class ThumbnailMakerService(object):
         num_images = len(os.listdir(self.input_dir))
 
         start = time.perf_counter()
-        for filename in os.listdir(self.input_dir):
-            orig_img = Image.open(self.input_dir + os.path.sep + filename)
-            for basewidth in target_sizes:
-                img = orig_img
-                # calculate target height of the resized image to maintain the aspect ratio
-                wpercent = (basewidth / float(img.size[0]))
-                hsize = int((float(img.size[1]) * float(wpercent)))
-                # perform resizing
-                img = img.resize((basewidth, hsize), PIL.Image.LANCZOS)
-                
-                # save the resized image to the output dir with a modified file name 
-                new_filename = os.path.splitext(filename)[0] + \
-                    '_' + str(basewidth) + os.path.splitext(filename)[1]
-                img.save(self.output_dir + os.path.sep + new_filename)
+        while True:
+            filename = self.img_queue.get()
+            if filename:
+                orig_img = Image.open(self.input_dir + os.path.sep + filename)
+                for basewidth in target_sizes:
+                    img = orig_img
+                    # calculate target height of the resized image to maintain the aspect ratio
+                    wpercent = (basewidth / float(img.size[0]))
+                    hsize = int((float(img.size[1]) * float(wpercent)))
+                    # perform resizing
+                    img = img.resize((basewidth, hsize), PIL.Image.LANCZOS)
+                    
+                    # save the resized image to the output dir with a modified file name 
+                    new_filename = os.path.splitext(filename)[0] + \
+                        '_' + str(basewidth) + os.path.splitext(filename)[1]
+                    img.save(self.output_dir + os.path.sep + new_filename)
+            else:
+                self.img_queue.task_done()
+                break
 
             os.remove(self.input_dir + os.path.sep + filename)
+            self.img_queue.task_done()
         end = time.perf_counter()
 
         logging.info("created {} thumbnails in {} seconds".format(num_images, end - start))
@@ -108,13 +87,24 @@ class ThumbnailMakerService(object):
         logging.info("START make_thumbnails")
         start = time.perf_counter()
 
-        self.download_images(img_url_list)
-        # self.perform_resizing()
+        for image_url in IMG_URLS:
+            self.dl_queue.put(image_url)
+        
+        num_dl_threads = 20
+        for _ in range(num_dl_threads):
+            t = Thread(target=self.download_image)
+            t.start()
+
+
+        t2 = Thread(target=self.perform_resizing)
+        t2.start()
+
+        self.dl_queue.join()
+        self.img_queue.put(None) #Poison pill
 
         end = time.perf_counter()
         logging.info("END make_thumbnails in {} seconds".format(end - start))
-
-
+    
 
 
 if __name__ == "__main__":
